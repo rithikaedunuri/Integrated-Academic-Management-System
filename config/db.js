43a6b5139704ffe6c48c0d609b29b01e1cb01fef
rithikaedunuri <rithikaedunuri@gmail.com>
2026-05-22 14:00:52 +0530

    Integrated Academic and Examination Management System

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/academic-management");
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log("Database Connection Error");
+        console.log(error);
+    }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Backend Working Successfully");
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
